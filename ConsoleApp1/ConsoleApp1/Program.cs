19c50c85bdcf7ce5515fe707f49e9bbc54d85d12
John <john_m_peck@yahoo.com>
2017-05-08 13:48:50 -0400

    First commit. Basic functioning is right. Repeatability broken.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var screenNumber = 0.0;
+            while (true)
+            {
+                Console.WriteLine("Give me a number, Hal.");
+                var entryOne = Console.ReadLine();
+                Console.WriteLine("Give me a simple arithmatic function: \"+\", \"-\", \"*\", \"/\"");
+                var aFunction = Console.ReadLine();
+                Console.WriteLine("Give me a second number, Hal.");
+                var entryTwo = Console.ReadLine();
+                var numberOne = double.Parse(entryOne);
+                var numberTwo = double.Parse(entryTwo);
+                if (aFunction == "+")
+                {
+                    screenNumber += (numberOne + numberTwo);
+                    Console.WriteLine("The answer is " + screenNumber + ". You can continue to calculate by entering a new arithmatic function or type \"clear\" to start over.");
+                    aFunction = Console.ReadLine();
+                    if (aFunction == "quit")
+                    {
+                        break;
+                    }
+                    else if (aFunction == "clear")
+                    {
+                        screenNumber = 0.0;
+                        continue;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                } else if (aFunction == "-")
+                {
+                    screenNumber += (numberOne - numberTwo);
+                    Console.WriteLine("The answer is " + screenNumber + ". You can continue to calculate by entering a new arithmatic function or type \"clear\" to start over.");
+                    aFunction = Console.ReadLine();
+                    if (aFunction == "quit")
+                    {
+                        break;
+                    }
+                    else if (aFunction == "clear")
+                    {
+                        screenNumber = 0.0;
+                        continue;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                } else if (aFunction == "*")
+                {
+                    screenNumber += (numberOne * numberTwo);
+                    Console.WriteLine("The answer is " + screenNumber + ". You can continue to calculate by entering a new arithmatic function or type \"clear\" to start over.");
+                    aFunction = Console.ReadLine();
+                    if (aFunction == "quit")
+                    {
+                        break;
+                    }
+                    else if (aFunction == "clear")
+                    {
+                        screenNumber = 0.0;
+                        continue;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                } else if (aFunction == "/")
+                {
+                    screenNumber += (numberOne / numberTwo);
+                    Console.WriteLine("The answer is " + screenNumber + ". You can continue to calculate by entering a new arithmatic function or type \"clear\" to start over.");
+                    aFunction = Console.ReadLine();
+                    if (aFunction == "quit")
+                    {
+                        break;
+                    } else if (aFunction == "clear")
+                    {
+                        screenNumber = 0.0;
+                        continue;
+                    } else
+                    {
+                        continue;
+                    }
+                } else
+
+                {
+                    Console.WriteLine("I'm working on this");
+                }
+            }
+
+        }
+    }
+}
