d4fca6a9307d0451309ffff5342853c3aaa8a361
John <john_m_peck@yahoo.com>
2017-05-08 19:35:06 -0400

    Made the loop repeatable and made code dry-er

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,93 +7,75 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             var screenNumber = 0.0;
+            var returnValue = 0.0;
+            var start = true;
+            string entryOne = null;
+            string entryTwo = null;
+            double numberOne = 0.0;
+            double numberTwo = 0.0;
+            var aFunction = "0";
             while (true)
             {
-                Console.WriteLine("Give me a number, Hal.");
-                var entryOne = Console.ReadLine();
-                Console.WriteLine("Give me a simple arithmatic function: \"+\", \"-\", \"*\", \"/\"");
-                var aFunction = Console.ReadLine();
-                Console.WriteLine("Give me a second number, Hal.");
-                var entryTwo = Console.ReadLine();
-                var numberOne = double.Parse(entryOne);
-                var numberTwo = double.Parse(entryTwo);
-                if (aFunction == "+")
+                if (start == true)
                 {
-                    screenNumber += (numberOne + numberTwo);
-                    Console.WriteLine("The answer is " + screenNumber + ". You can continue to calculate by entering a new arithmatic function or type \"clear\" to start over.");
+                    Console.WriteLine("Give me a number, Hal.");
+                    entryOne = Console.ReadLine();
+                    Console.WriteLine("Give me a simple arithmatic function: \"+\", \"-\", \"*\", \"/\"");
                     aFunction = Console.ReadLine();
-                    if (aFunction == "quit")
-                    {
-                        break;
-                    }
-                    else if (aFunction == "clear")
-                    {
-                        screenNumber = 0.0;
-                        continue;
-                    }
-                    else
-                    {
-                        continue;
-                    }
-                } else if (aFunction == "-")
-                {
-                    screenNumber += (numberOne - numberTwo);
-                    Console.WriteLine("The answer is " + screenNumber + ". You can continue to calculate by entering a new arithmatic function or type \"clear\" to start over.");
-                    aFunction = Console.ReadLine();
-                    if (aFunction == "quit")
-                    {
-                        break;
-                    }
-                    else if (aFunction == "clear")
-                    {
-                        screenNumber = 0.0;
-                        continue;
-                    }
-                    else
-                    {
-                        continue;
-                    }
-                } else if (aFunction == "*")
-                {
-                    screenNumber += (numberOne * numberTwo);
-                    Console.WriteLine("The answer is " + screenNumber + ". You can continue to calculate by entering a new arithmatic function or type \"clear\" to start over.");
-                    aFunction = Console.ReadLine();
-                    if (aFunction == "quit")
-                    {
-                        break;
-                    }
-                    else if (aFunction == "clear")
-                    {
-                        screenNumber = 0.0;
-                        continue;
-                    }
-                    else
-                    {
-                        continue;
-                    }
-                } else if (aFunction == "/")
-                {
-                    screenNumber += (numberOne / numberTwo);
-                    Console.WriteLine("The answer is " + screenNumber + ". You can continue to calculate by entering a new arithmatic function or type \"clear\" to start over.");
-                    aFunction = Console.ReadLine();
-                    if (aFunction == "quit")
-                    {
-                        break;
-                    } else if (aFunction == "clear")
-                    {
-                        screenNumber = 0.0;
-                        continue;
-                    } else
-                    {
-                        continue;
-                    }
-                } else
-
-                {
-                    Console.WriteLine("I'm working on this");
+                    start = false;
+                    continue;
                 }
-            }
+                else
+                {
+                    Console.WriteLine("Give me a second number, Hal.");
+                    entryTwo = Console.ReadLine();
+                    numberOne = double.Parse(entryOne);
+                    numberTwo = double.Parse(entryTwo);
+                    if (aFunction == "+")
+                    {
+                        screenNumber = (numberOne + numberTwo);
+                    }
+                    else if (aFunction == "-")
+                    {
+                        screenNumber = (numberOne - numberTwo);
+                    }
+                    else if (aFunction == "*")
+                    {
+                        screenNumber = (numberOne * numberTwo);
+                    }
+                    else if (aFunction == "/")
+                    {
+                        screenNumber = (numberOne / numberTwo);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid arithmatic function.");
+                        Console.ReadLine();
+                    }
 
+                    Console.WriteLine("The answer is " + screenNumber + ". You can continue to calculate by entering a new arithmatic function or type \"clear\" to start over.");
+                    aFunction = Console.ReadLine();
+                    if (aFunction == "quit")
+                    {
+                        break;
+                    }
+                    else if (aFunction == "clear")
+                    {
+                        screenNumber = 0.0;
+                        start = true;
+                        continue;
+                    }
+                    else
+                    {
+                        entryOne = screenNumber.ToString();
+                        start = false;
+                        continue;
+                    }
+
+
+                }
+
+            }
         }
     }
 }
